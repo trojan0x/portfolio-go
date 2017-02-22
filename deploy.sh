35492914ad85a7d0c6fda2d9efe35f09c12a363f
Daniel Rodríguez <daniel@dpstudios.es>
2017-02-22 22:57:16 +0100

    :sparkles: Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git fetch --all
 git reset --hard origin/master
 
 docker-compose -f docker-compose-prod.yml build
-docker-compose -f docker-compose-prod.yml restart
+docker-compose -f docker-compose-prod.yml up -d
